540b2a200ab7c4be9583a19c6cd1f040cfa82333
Tekislla <jplazarim@gmail.com>
2022-08-06 14:18:26 -0300

    Atualização de repo

--- Eclipse Workspaces/training-application/exemplo/src/main/java/com/restaurant/pizza/repository/PizzaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.pizza.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.restaurant.pizza.entities.Pizza;
-
-public interface PizzaRepository extends CrudRepository<Pizza, Long> {
-
-}
